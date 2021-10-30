745c664887e54116db129de52616089170e6f398
Ofir Gan <offirgan@gmail.com>
2021-10-30 23:05:45 +0300

    Fix Folders

--- Homework3/modules/app/s3.tf
+++ Homework3/modules/app/s3.tf
@@ -11,6 +11,8 @@ resource "aws_s3_bucket" "web_servers_logs_bucket" {
   bucket   = lower("${var.purpose_tag}-${var.s3_logs_bucket_name}")
   acl      = "private"
 
+  force_destroy = true
+
   tags = {
     Name = lower("${var.purpose_tag}-${var.s3_logs_bucket_name}")
   }
