ab4d9a8cbf0912e932235bb5e5a8bc7eb2146ffe
Ofir Gan <offirgan@gmail.com>
2021-11-16 00:38:44 +0200

    update vpc module version

--- Homework4/ec2/main.tf
+++ Homework4/ec2/main.tf
@@ -1,16 +1,30 @@
 ##################################################################################
+# DATA SOURCE
+##################################################################################
+
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "OpsSchool-OfirGan"
+    workspaces = {
+      name = "vpc-workspace"
+    }
+  }
+}
+
+##################################################################################
 # EC2
 ##################################################################################
 
 module "ec2" {
   source               = "app.terraform.io/OpsSchool-OfirGan/ec2/aws"
   version              = "0.1.0-alpha"
-  vpc_id               = module.vpc.vpc_id
-  available_zone_names = module.vpc.available_zone_names
+  vpc_id               = data.terraform_remote_state.vpc.outputs.vpc_id
+  available_zone_names = data.terraform_remote_state.vpc.outputs.available_zone_names
   s3_logs_bucket_name  = var.s3_logs_bucket_name
   s3_logs_folder       = var.s3_logs_folder
-  public_subnets_ids   = module.vpc.public_subnets_ids
-  private_subnets_ids  = module.vpc.private_subnets_ids
+  public_subnets_ids   = data.terraform_remote_state.vpc.outputs.public_subnets_ids
+  private_subnets_ids  = data.terraform_remote_state.vpc.outputs.private_subnets_ids
   instance_count       = var.instance_count
   instance_type_web    = var.instance_type["web"]
   instance_type_db     = var.instance_type["db"]
