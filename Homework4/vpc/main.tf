e070c64b136f76abf36e5427f9b0e43dbc4d4427
Ofir Gan <offirgan@gmail.com>
2021-11-16 16:07:23 +0200

    terraform upgrade to 1.0.11

--- Homework4/vpc/main.tf
+++ Homework4/vpc/main.tf
@@ -4,7 +4,7 @@
 
 module "vpc" {
   source                   = "app.terraform.io/OpsSchool-OfirGan/vpc/aws"
-  version                  = "0.2.0-alpha"
+  version                  = "0.4.0-alpha"
   purpose_tag              = var.purpose_tag
   vpc_cidr                 = var.vpc_cidr
   availability_zones_count = var.availability_zones_count
--- Homework4/vpc/providers.tf
+++ Homework4/vpc/providers.tf
@@ -3,7 +3,7 @@
 ##################################################################################
 
 terraform {
-  required_version = "1.0.9"
+  required_version = "1.0.11"
 
   backend "remote" {
     organization = "OpsSchool-OfirGan"
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.63.0"
+      version = "3.65.0"
     }
   }
 }
