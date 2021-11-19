57a79db149eb6b84af9e4168293d0100f7542e7e
Ofir Gan <offirgan@gmail.com>
2021-11-19 11:55:32 +0200

    fix outputs - now terraforrm is working

--- Homework4/ec2/outputs.tf
+++ Homework4/ec2/outputs.tf
@@ -1,11 +1,11 @@
 output "db_servers_private_ips" {
-  value = module.app.db_servers_private_ips
+  value = module.ec2.db_servers_private_ips
 }
 
 output "web_servers_public_ips" {
-  value = module.app.web_servers_public_ips
+  value = module.ec2.web_servers_public_ips
 }
 
 output "alb_public_dns" {
-  value = module.app.alb_public_dns
+  value = module.ec2.alb_public_dns
 }
